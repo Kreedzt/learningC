096e9da248a2382718af1d88e26a2e31754156d9
Kreedzt <zhaozisong1@live.com>
2019-09-30 16:36:53 +0800

    Kreedzt - complete chapter9 summary

---
+++ practice/9/examples/swap3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void interchange(int *u, int *v);
+
+int main()
+{
+    int x = 5, y = 10;
+
+    printf("Originally x = %d and y = %d.\n", x, y);
+
+    interchange(&x, &y); // 把地址发送给函数
+
+    printf("Now x = %d and y = %d.\n", x, y);
+
+    return 0;
+}
+
+void interchange(int *u, int *v)
+{
+    int temp;
+    temp = *u; // temp获得u所指向对象的值
+    *u = *v;
+    *v = temp;
+}
