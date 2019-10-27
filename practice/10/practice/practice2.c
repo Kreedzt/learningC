2176dfe52c93c4d70f07df67aab3bcc62dff14bf
Kreedzt <zhaozisong1@live.com>
2019-10-27 22:53:56 +0800

    Kreedzt - complete practice1 at 10.13

---
+++ practice/10/practice/practice2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void copy_arr(double target[], double source[], int length);
+void copy_ptr(double target[], double source[], int length);
+void copy_ptrs(double target[], double source[], int length);
+
+int main(int argc, char *argv[])
+{
+  double source[5] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+  double target1[5];
+  double target2[5];
+  double target3[5];
+
+  
+  
+  
+  return 0;
+}
+
+void copy_arr(double target[], double source[], int length)
+{
+  for (int i = 0; i < n; ++i) {
+    target[i] = source[i];
+  }
+}
+
+void copy_ptr(double target[], double source[], int length)
+{
+  
+}
+
+void copy_ptrs(double target[], double source[], int length)
+{
+  
+}
