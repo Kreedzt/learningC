0c125163df6fbf3a08f48408d1d6da91dabdbe89
Kreedzt <zhaozisong1@live.com>
2019-11-14 22:12:35 +0800

    Kreedzt - completed summary to 11.5.4

---
+++ practice/11/examples/starsrch.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+#define LISTSIZE 6
+
+int main(int argc, char *argv[])
+{
+
+  const char* list[LISTSIZE] = {
+    "astronomy", "astounding",
+    "astrophysics", "ostracize",
+    "asterism", "astrophobia"
+  };
+
+  int count = 0;
+  int i;
+
+  for (i = 0; i < LISTSIZE; ++i) {
+    if (strncmp(list[i], "astro", 5) == 0) {
+      printf("Found:  %s\n", list[i]);
+      count++;
+    }
+
+  }
+
+  printf("The list contained %d wors beginning"
+         "  with astro.\n", count);
+
+  
+  return 0;
+}
+
