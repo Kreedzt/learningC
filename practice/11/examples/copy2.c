c5d88307f563fc1d8334acc5e5ef04be18645159
Kreedzt <zhaozisong1@live.com>
2019-11-18 16:30:11 +0800

    Kreedzt - completed code to 11.5.6

---
+++ practice/11/examples/copy2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+#define WORDS "beast"
+#define SIZE 40
+
+int main()
+{
+  const char* orig = WORDS;
+  char copy[SIZE] = "Be the best that you can be.";
+  char* ps;
+
+  puts(orig);
+  puts(copy);
+
+  // 拷贝前:
+  // Be the best that you can be.\0
+  // 空字符也拷贝
+  ps = strcpy(copy + 7, orig);
+
+  // 拷贝后字符:
+  // Be the besast\0hat you can be.\0
+
+  puts(copy);
+  puts(ps);
+  
+
+  return 0;
+}
