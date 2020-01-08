945a0d47bad864e2514dd3fde7cccb67ac583815
Kreedzt <zhaozisong1@live.com>
2020-01-08 21:22:56 +0800

    Kreedzt - completed summary to 12.4 einleitung

---
+++ practice/12/examples/dyn_arr.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  double* ptd;
+  int max;
+  int number;
+  int i = 0;
+
+  puts("What is the maximum number of type double entries?");
+
+  if (scanf("%d", &max) !=1)
+  {
+    puts("Number not correctly entered -- bye.");
+    exit(EXIT_FAILURE);
+  }
+
+  ptd = (double*)malloc(max * sizeof(double));
+
+  if (ptd == NULL)
+  {
+    puts("Memory allocation failed. Goodbye.");
+    exit(EXIT_FAILURE);
+  }
+
+  // ptd现在指向由max个元素的数组
+  puts("Enter the values (q to quit):");
+
+  while (i < max && scanf("%lf", &ptd[i]) == 1) {
+    ++i;
+  }
+  printf("Here are your %d entries:\n", number = i);
+
+  for (i = 0; i < number; ++i) {
+    printf("%7.2f ", ptd[i]);
+
+    if (i % 7 == 6)
+      putchar('\n');
+  }
+
+  if (i % 7 != 0)
+    putchar('\n');
+    
+  puts("Done");
+  free(ptd);
+  
+  return 0;
+}
