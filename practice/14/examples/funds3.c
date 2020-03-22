e1f5445d643a1233eb8b92407f7f9087c3a2505e
Kreedzt <zhaozisong1@live.com>
2020-03-22 21:52:06 +0800

    :sparkles: Kreedzt - completed summary to 14.7.3

---
+++ practice/14/examples/funds3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define FUNDLEN 50
+
+struct funds
+{
+  char bank[FUNDLEN];
+  double bankfund;
+  char save[FUNDLEN];
+  double savefund;
+};
+
+double sum(struct funds moolah);
+
+int main(int argc, char *argv[])
+{
+  struct funds stan =  {
+    "Garlic-Melon Bank",
+    4032.27,
+    "Lucky's Savings and Loan",
+    8543.94
+  };
+
+  printf("Stan has a total of $%.2f\n",
+         sum(stan));
+  
+  return 0;
+}
+
+double sum(struct funds moolah)
+{
+  return (moolah.bankfund + moolah.savefund);
+}
