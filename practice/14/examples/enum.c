d89f8ecc0db334397f2892437e5ed5fbf512a972
Kreedzt <zhaozisong1@live.com>
2020-03-25 18:38:11 +0800

    :sparkles: Kreedzt - completed summary to 14.14

---
+++ practice/14/examples/enum.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h> // C99 特性
+
+char* s_gets(char* st, int n);
+
+enum spectrum { red, orange, yellow, green, blue, violet };
+
+const char* colors[] = { "red", "orange", "yellow", "green", "blue", "violet" };
+#define LEN 30
+
+int main(int argc, char *argv[])
+{
+  char choice[LEN];
+  enum spectrum color;
+  bool color_is_found = false;
+  
+  puts("Enter a color (empty line to quit):");
+
+  while (s_gets(choice, LEN) != NULL && choice[0] != '\0') {
+    for (color = red; color <= violet; ++color) {
+      if (strcmp(choice, colors[color]) == 0) {
+        color_is_found = true;
+        break;
+      }
+    }
+
+    if (color_is_found) {
+      switch (color) {
+        case red: {
+          puts("Roses are red.");
+          break;
+        }
+        case yellow:
+          puts("Sumflowers are yellow.");
+          break;
+        case green:
+          puts("Grass is green.");
+          break;
+        case blue:
+          puts("Bluebells are blue.");
+          break;
+        case violet:
+          puts("Violets are violet.");
+          break;
+        default:
+          break;
+      }
+    } else {
+      printf("I don't know about the color %s.\n", choice);
+    }
+
+    color_is_found = false;
+    puts("Next color, please(empty line to quit:)");
+  }
+
+  puts("Goodbye!");
+  
+  return 0;
+}
+
+char* s_gets(char* st, int n)
+{
+  char* ret_val;
+  char* find;
+  ret_val = fgets(st, n, stdin);
+  
+  if (ret_val) {
+    find = strchr(st, '\n'); // 查找换行符
+
+    // 如果地址不是NULL
+    if (find) {
+      *find = '\0'; // 在此处放置一个空字符
+    } else {
+      while (getchar() != '\n') {
+        continue; // 清理输入行
+      }
+    }
+  }
+
+  return ret_val;
+}
