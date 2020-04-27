67d6045b568282cf6be10d5c7b3b16db57292415
Kreedzt <zhaozisong1@live.com>
2020-04-27 23:58:56 +0800

    :sparkles: Kreedzt - completed summary to 16.12.1

---
+++ practice/16/examples/assert.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+int main()
+{
+  double x, y, z;
+  puts("Enter a pair of numbers(0 0 to quit):");
+
+  while(scanf("%lf %lf", &x, &y) == 2
+        && (x != 0 || y != 0)){
+    z = x * x - y * y; // 应该用 +
+    assert(z >= 0);
+
+    printf("answer is %f\n", sqrt(z));
+    puts("Next pair of numbers:");
+  }
+
+  puts("Done");
+  return 0;
+}
