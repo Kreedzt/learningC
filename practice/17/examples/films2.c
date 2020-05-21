24fc2881265e12ea107b7937d0d4fee30c8bb030
Kreedzt <zhaozisong1@live.com>
2020-05-21 17:14:17 +0800

    :sparkles: Kreedzt - completed summary to 17.2

---
+++ practice/17/examples/films2.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TSIZE 45
+
+struct film {
+  char title[TSIZE];
+  int rating;
+  struct film* next;
+};
+
+char* s_gets(char* st, int n);
+
+int main()
+{
+  struct film* head = NULL;
+  struct film *prev, *current;
+  char input[TSIZE];
+
+  puts("Enter first movie title:");
+
+  while (s_gets(input, TSIZE) != NULL && input[0] != '\0')
+  {
+    current = (struct film*)malloc(sizeof(struct film));
+
+    if (head == NULL) {
+      head = current;
+    } else {
+      prev->next = current;
+    }
+
+    current->next = NULL;
+
+    strcpy(current->title, input);
+
+    puts("Enter your rating <0-10>:");
+    scanf("%d", &current->rating);
+
+    while(getchar() != '\n'){
+      continue;
+    }
+
+    puts("Enter next movie title(compty line to stop)");
+
+    prev = current;
+  }
+
+  // 显示电影列表
+
+  if (head == NULL) {
+    printf("No data entered");
+  } else {
+    printf("Here is the movie list:\n");
+    current = head;
+
+    while (current != NULL) {
+      printf("Movie: %s Rating: %d\n",
+             current->title,
+             current->rating);
+
+      current = current->next;
+    }
+  }
+
+  printf("Displayed Completed.\n");
+
+  // 完成任务, 释放已分配的内存
+  current = head;
+  while (current != NULL) {
+    // current = head;
+    // head = current->next;
+    // printf("Entered loop\n");
+    // free(head);
+    
+    // 示例中代码有误, 改为正确释放逻辑(MSVC上)
+    printf("Entered loop\n");
+    head = current;
+    current = current->next;
+    free(head);
+  }
+
+  printf("Bye!\n");
+
+  return 0;
+}
+
+char* s_gets(char* st, int n)
+{
+  char* ret_val;
+  char* find;
+  ret_val = fgets(st, n, stdin);
+
+  if (ret_val)
+  {
+    find = strchr(st, '\n');
+
+    if (find) {
+      *find = '\0';
+    } else {
+      while (getchar() != '\n') {
+        continue;
+      }
+    }
+  }
+
+  return ret_val;
+}
